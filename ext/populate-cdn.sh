8e7e1bd4f9e9335542d6d006b58d8d0692df58da
wavesoft <wavesoft@github.com>
2014-11-26 19:52:02 +0100

    Adding missing CDN entries

--- ext/populate-cdn.sh
+++ ext/populate-cdn.sh
@@ -9,15 +9,21 @@
 # Bootstrap
 wget -c -O css/bootstrap.min.css http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css
 wget -c -O js/bootstrap.min.js http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js
-wget -c -O css/images/ui-bg_flat_75_ffffff_40x100.png http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/images/ui-bg_flat_75_ffffff_40x100.png
-wget -c -O css/images/ui-bg_glass_75_e6e6e6_1x400.png http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/images/ui-bg_glass_75_e6e6e6_1x400.png
-wget -c -O css/images/ui-bg_glass_75_dadada_1x400.png http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/images/ui-bg_glass_75_dadada_1x400.png
+wget -c -O fonts/glyphicons-halflings-regular.eot http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/fonts/glyphicons-halflings-regular.eot
+wget -c -O fonts/glyphicons-halflings-regular.svg http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/fonts/glyphicons-halflings-regular.svg
+wget -c -O fonts/glyphicons-halflings-regular.ttf http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/fonts/glyphicons-halflings-regular.ttf
+wget -c -O fonts/glyphicons-halflings-regular.woff http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/fonts/glyphicons-halflings-regular.woff
 
 # jQuery
 wget -c -O js/jquery-1.11.0.min.js http://code.jquery.com/jquery-1.11.0.min.js
 
 # jQuery UI
 wget -c -O css/jquery-ui.css http://code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css
+wget -c -O css/images/ui-bg_glass_75_e6e6e6_1x400.png http://code.jquery.com/ui/1.11.1/themes/smoothness/images/ui-bg_glass_75_e6e6e6_1x400.png
+wget -c -O css/images/ui-bg_glass_75_dadada_1x400.png http://code.jquery.com/ui/1.11.1/themes/smoothness/images/ui-bg_glass_75_dadada_1x400.png
+wget -c -O css/images/ui-bg_glass_65_ffffff_1x400.png http://code.jquery.com/ui/1.11.1/themes/smoothness/images/ui-bg_glass_65_ffffff_1x400.png
+wget -c -O css/images/ui-bg_flat_75_ffffff_40x100.png http://code.jquery.com/ui/1.11.1/themes/smoothness/images/ui-bg_flat_75_ffffff_40x100.png
+
 wget -c -O js/jquery-ui.js http://code.jquery.com/ui/1.11.1/jquery-ui.js
 
 # HTML5 compatibility
