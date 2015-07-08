a5c053d42ef80b3a2b73c2eaf117189310785a84
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-07-08 18:42:25 +0200

    Changing title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>CernVM WebAPI</title>
+		<title>CERN Challenge Dashboard</title>
 
 		<!-- OpenGraph Properties -->
 		<meta property="og:title" content="The CERN Challenge" />
